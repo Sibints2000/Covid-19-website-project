5cae2c93955a8246a7bebdd2473b0e6d77142b3a
Sibints2000 <sibints2000@gmail.com>
2021-09-07 20:38:18 +0530

    H-Eighteenth Commit

--- script.js
+++ script.js
@@ -28,15 +28,34 @@ $(document).ready(function () {
 
             let top = $(window).scrollTop();
             let offset = $(this).offset().top - 200;
-            let id = $().attr('id');
-            let height = $().height();
+            let id = $(this).attr('id');
+            let height = $(this).height();
+
+            if(top > offset && top < offset + height) {
+                $('.navbar a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
 
         });
 
 
     });
 
+    //smooth scrolling
 
+    $('a[href*="#"]').on('click', function(e) {
+
+        $('html, body').animate({
+
+            scrollTop : $($(this).attr('href')).offset().top,
+
+        },
+          500,
+          'linear'        
+
+        );
+
+    });
 
 
 });
